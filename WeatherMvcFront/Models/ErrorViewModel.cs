fc8e2e13c26aa6c4269542595c3f068377a138ff
sailorspace <sanjaymahara@gmail.com>
2024-09-03 09:43:18 +0530

    Add mvc project tha twould call backendapi

---
+++ WeatherMvcFront/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherMvcFront.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
